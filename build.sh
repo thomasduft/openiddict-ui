a9c0c3016fb8c4808d8bc85d4564b0249301c1be
thomasduft <thomas.duft@gmail.com>
2020-11-11 15:50:57 +0100

    provided working build.sh file

--- build.sh
+++ build.sh
@@ -12,14 +12,13 @@ echo Cleaning up
 rm -r ./dist
 
 echo Restore solution
-dotnet restore microwf.sln
+dotnet restore openiddict-ui.sln
 
 version = $1
 echo Packaging solution with PackageVersion = $1 and Version = $1
-# dotnet pack src/microwf.Core -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
-# dotnet pack src/microwf.Domain -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
-# dotnet pack src/microwf.Infrastructure -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
-# dotnet pack src/microwf.AspNetCoreEngine -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
+dotnet pack src/OpenIddict.UI.Core -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
+dotnet pack src/OpenIddict.UI.Infrastructure -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
+dotnet pack src/OpenIddict.UI.Api -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
 
 if [ -z "$2" ]
 then
