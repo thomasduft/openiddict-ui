62c27d39ce33d2a24039eae3601dffd8be7b5a34
thomasduft <thomas.duft@gmail.com>
2020-11-19 19:01:47 +0000

    improved build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$1" ];
 then
   echo No version specified! Please specify a valid version like 1.2.3!
   exit 1
-else
-  echo version $1
 fi
 
+if [ -z "$2" ];
+then
+  echo No release info specified! For a relase provide the r flag and a valid release verion!
+  exit 1
+fi
+
+echo ----
+echo Starting building version $1
+
+echo ----
 echo Cleaning up
 rm -r ./dist
 
+echo ----
 echo Restore solution
 dotnet restore openiddict-ui.sln
 
-version = $1
-
-if [ -n "$2" && $2 = "r" ]
+echo ----
+if [ $2 = "r" ];
 then
   echo Packaging solution with Version = $1
   dotnet pack src/OpenIddict.UI.Core -c Release -p:PackageVersion=$1 -p:Version=$1 -o ./dist/nupkgs/
@@ -29,19 +37,23 @@ else
   dotnet pack src/OpenIddict.UI.Api -c Release -p:PackageVersion=$1 -o ./dist/nupkgs/
 fi
 
-if [ -z "$3" ]
+if [ -z "$3" ];
 then
+  echo ----
   echo Done
   exit 0
 fi
 
+echo ----
+echo Pushing packages
 for package in $(find ./dist/nupkgs/ -name *.nupkg); do
-  echo Pushing $package
   dotnet nuget push $package -k $3 -s https://api.nuget.org/v3/index.json
 done
 
+echo ----
 echo Creating and pushing tag 
 git tag -a v$1 -m '"version '$1'"'
 git push origin v$1
 
+echo ----
 echo Done
