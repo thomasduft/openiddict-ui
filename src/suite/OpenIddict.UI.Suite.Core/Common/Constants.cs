2097b61a07d086c20647308c1cda1abf518a3197
thomasduft <thomas.duft@gmail.com>
2021-12-13 20:17:38 +0000

    removed commented code

--- src/suite/OpenIddict.UI.Suite.Core/Common/Constants.cs
+++ src/suite/OpenIddict.UI.Suite.Core/Common/Constants.cs
@@ -1,10 +1,5 @@
 namespace tomware.OpenIddict.UI.Suite.Core
 {
-  // public static class Policies
-  // {
-  //   public const string ADMIN_POLICY = "AdministratorPolicy";
-  // }
-
   public static class Roles
   {
     public const string ADMINISTRATOR_ROLE = "Administrator";
