416af023e91e0a45c0982a2fab0a2d7994d2f20a
thomasduft <thomas.duft@gmail.com>
2022-03-08 13:07:17 +0000

    improved logging in Sample Server

--- samples/Server/Program.cs
+++ samples/Server/Program.cs
@@ -1,30 +1,56 @@
-﻿using Server;
+﻿using Serilog;
+using Server;
 using Server.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-var environmentName = builder.Environment.EnvironmentName;
 
-// Configure services
-builder.Services.AddServer(configuration, environmentName);
+var loggerConfiguration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
-// Configure application pipeline
-var app = builder.Build();
-app.UseServer(environmentName);
+Log.Logger = new LoggerConfiguration()
+              .ReadFrom.Configuration(loggerConfiguration)
+              .CreateLogger();
 
-// Ensure DB migrations
-using (var scope = app.Services.CreateScope())
+try
 {
-  var services = scope.ServiceProvider;
-  try
+  Log.Information("Starting web host");
+
+  var builder = WebApplication.CreateBuilder(args);
+  var configuration = builder.Configuration;
+  var environmentName = builder.Environment.EnvironmentName;
+
+  // Configure services
+  builder.Services.AddServer(configuration, environmentName);
+
+  // Configure application pipeline
+  var app = builder.Build();
+  app.UseServer(environmentName);
+
+  // Ensure DB migrations
+  using (var scope = app.Services.CreateScope())
   {
-    var migrator = services.GetRequiredService<IMigrationService>();
-    migrator.EnsureMigrationAsync().GetAwaiter().GetResult();
+    var services = scope.ServiceProvider;
+    try
+    {
+      var migrator = services.GetRequiredService<IMigrationService>();
+      migrator.EnsureMigrationAsync().GetAwaiter().GetResult();
+    }
+    catch (Exception ex)
+    {
+      app.Logger.LogError(ex, "An error occurred while migrating the database.");
+    }
   }
-  catch (Exception ex)
-  {
-    app.Logger.LogError(ex, "An error occurred while migrating the database.");
-  }
+
+  app.Run();
+}
+catch (Exception ex)
+{
+  Log.Fatal(ex, "Host terminated unexpectedly");
+  return 1;
+}
+finally
+{
+  Log.CloseAndFlush();
 }
 
-app.Run();
+return 0;
