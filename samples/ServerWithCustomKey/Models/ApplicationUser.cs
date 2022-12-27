ea1f463f452d98c2594eb76227a8b4133c04d98e
thomasduft <thomas.duft@gmail.com>
2022-12-27 09:17:11 +0000

    removed ServerWithCustomKey sample

--- samples/ServerWithCustomKey/Models/ApplicationUser.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace ServerWithCustomKey.Models;
-
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser<int> { }
