dc26fddfe69143c7573452dbc9120fe2a3128ec7
thomasduft <thomas.duft@gmail.com>
2023-08-10 16:06:21 +0000

    updated to latest nuget dependencies

--- samples/Server/ConfigureServices.cs
+++ samples/Server/ConfigureServices.cs
@@ -63,7 +63,6 @@ public static class ConfigureServices
       // (like pruning orphaned authorizations/tokens from the database) at regular intervals.
       services.AddQuartz(options =>
       {
-        options.UseMicrosoftDependencyInjectionJobFactory();
         options.UseSimpleTypeLoader();
         options.UseInMemoryStore();
       });
